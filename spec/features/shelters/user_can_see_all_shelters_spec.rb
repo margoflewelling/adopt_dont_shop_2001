9ac4ea55370e982e2d437c504ea1c776786064b3
Margo Flewelling <flewelling.margo@gmail.com>
2020-03-17 16:01:40 -0600

    Add testing for index shelter page, add show method/view, and create the db

---
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "shelters index page", type: :feature do
+  it "can see all shelters names and address" do
+    shelter_1 = Shelter.create(name: "Routt Humane",
+                              address: "760 Critter Court",
+                              city: "Steamboat Springs",
+                              state: "CO",
+                              zip: "80487")
+
+    visit "/shelters"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_1.address)
+    expect(page).to have_content(shelter_1.city)
+    expect(page).to have_content(shelter_1.state)
+    expect(page).to have_content(shelter_1.zip)
+  end
+end
