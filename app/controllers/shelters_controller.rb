8d630e6e88a4d67fff7a831a80cfca50f7a67f7e
Margo Flewelling <flewelling.margo@gmail.com>
2020-03-17 16:49:42 -0600

    Add functionality for editing and deleting

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -15,11 +15,32 @@ class SheltersController < ApplicationController
       })
 
       shelter.save
-      redirect_to './shelters'
+      redirect_to '/shelters'
   end
 
   def show
     @shelter = Shelter.find(params[:id])
   end
+
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update({name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip],
+      })
+      shelter.save
+      redirect_to "/shelters/#{shelter.id}"
+  end
+
+  def destroy
+    Shelter.destroy(params[:id])
+    redirect_to '/shelters'
+  end 
 
 end
