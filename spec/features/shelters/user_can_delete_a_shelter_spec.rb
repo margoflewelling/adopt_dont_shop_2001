044b7be3cdc062bea301bb7876a38ab14620eb1a
Margo Flewelling <flewelling.margo@gmail.com>
2020-03-17 21:50:13 -0600

    Add test for destroying shelters

---
+++ spec/features/shelters/user_can_delete_a_shelter_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "destroy a shelter", type: :feature do
+  it "can delete a shelter" do
+    shelter_1 = Shelter.create(name: "Routt Humane",
+                              address: "760 Critter Court",
+                              city: "Steamboat Springs",
+                              state: "CO",
+                              zip: "80487")
+    shelter_2 = Shelter.create(name: "Dumb Friends League",
+                              address: "2080 S Quebec St",
+                              city: "Denver",
+                              state: "CO",
+                              zip: "80231")
+
+
+    visit "/shelters/#{shelter_2.id}"
+    expect { click_on "Delete Shelter" }.to change(Shelter, :count).by(-1)
+  end
+end
