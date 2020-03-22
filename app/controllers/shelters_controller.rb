143e193450741b791954b8a8a63205fe468357f3
Margo Flewelling <flewelling.margo@gmail.com>
2020-03-21 22:35:10 -0600

    Change shelter params to private

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -7,13 +7,7 @@ class SheltersController < ApplicationController
   end
 
   def create
-    shelter = Shelter.new({name: params[:name],
-      address: params[:address],
-      city: params[:city],
-      state: params[:state],
-      zip: params[:zip]
-      })
-
+    shelter = Shelter.new(shelter_params)
       shelter.save
       redirect_to '/shelters'
   end
@@ -28,13 +22,7 @@ class SheltersController < ApplicationController
 
   def update
     shelter = Shelter.find(params[:id])
-    shelter.update({name: params[:name],
-      address: params[:address],
-      city: params[:city],
-      state: params[:state],
-      zip: params[:zip]
-      })
-      shelter.save
+    shelter.update(shelter_params)
       redirect_to "/shelters/#{shelter.id}"
   end
 
@@ -45,6 +33,12 @@ class SheltersController < ApplicationController
 
   def pets
     @shelter = Shelter.find(params[:id])
+  end
+
+private
+
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
   end
 
 end
